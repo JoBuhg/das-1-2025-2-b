4134e2a9b95556b3ccfd21dcb738b2be82ba1bdb
JoBuhg <joaovitorfb4@gmail.com>
2025-09-11 23:18:19 +0000

    feat: adicionando primeira classe de tópico

---
+++ eventos/src/main/java/br/univille/publisher.java
@@ -0,0 +1,9 @@
+package br.univille;
+
+public class publisher {
+    public static void main(String[] args){
+        var topicname:"topic-das1-b";
+        var servidor ="https://sbdas12025a.servicebus.windows.net/topic-das1-b";
+        
+    }
+}
