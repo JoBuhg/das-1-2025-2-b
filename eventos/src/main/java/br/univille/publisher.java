b4fec5791d657a40a4e1ff2a18732fef20376dd1
JoBuhg <joaovitorfb4@gmail.com>
2025-09-11 23:22:08 +0000

    files: esqueci um kkkk

--- eventos/src/main/java/br/univille/publisher.java
+++ eventos/src/main/java/br/univille/publisher.java
@@ -1,9 +1,14 @@
 package br.univille;
 
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+
 public class publisher {
     public static void main(String[] args){
         var topicname:"topic-das1-b";
         var servidor ="https://sbdas12025a.servicebus.windows.net/topic-das1-b";
         
+        DefaultAzureCredential credential =
+            new DefaultAzureCredentialBuilder().build();
     }
 }
