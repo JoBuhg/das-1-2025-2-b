b49bf37509a7164466c4defd8f532ae078a3e486
JoBuhg <joaovitorfb4@gmail.com>
2025-09-15 23:31:58 +0000

    feat: adding the publisher logic for messagery

--- eventos/src/main/java/br/univille/publisher.java
+++ eventos/src/main/java/br/univille/publisher.java
@@ -1,14 +1,32 @@
 package br.univille;
 
+import com.azure.core.amqp.AmqpTransportType;
 import com.azure.identity.DefaultAzureCredential;
 import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
 
 public class publisher {
     public static void main(String[] args){
-        var topicname:"topic-das1-b";
-        var servidor ="https://sbdas12025a.servicebus.windows.net/topic-das1-b";
+        var topicName = "topic-das1-b";
+        var servidor = "sbdas12025a.servicebus.windows.net";
+                
+        // DefaultAzureCredential credential =
+        //     new DefaultAzureCredentialBuilder().build();    
+        String chave = System.getenv("CHAVE");
         
-        DefaultAzureCredential credential =
-            new DefaultAzureCredentialBuilder().build();
+        ServiceBusSenderClient SenderClient = new ServiceBusClientBuilder()
+            .fullyQualifiedNamespace(servidor)
+            .transportType(AmqpTransportType.AMQP_WEB_SOCKETS)
+            // .credential(credential)
+            .connectionString(chave)
+            .sender()
+            .topicName(topicName)
+            .buildClient();
+
+        SenderClient.sendMessage(
+            new ServiceBusMessage(
+                "joao: iniciando duplicação de identidade: RAMATTRA"));
     }
 }
