24ebf05af26d73d4ee0882a245387bf2f0454534
JoBuhg <joaovitorfb4@gmail.com>
2025-11-13 23:26:31 +0000

    feat- demo1 e demo2 microserviços

---
+++ demo1/src/main/java/br/univille/demo1/controller/HomeController.java
@@ -0,0 +1,28 @@
+package br.univille.demo1.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.domain.HttpExtension;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HomeController {
+
+    @GetMapping("/startASync")
+    public ResponseEntity startASync(){
+
+        try(DaprClient client = new DaprClientBuilder().build()){
+            var mensagem = "Hello from demo1";
+            client.invokeMethod("demo2", "/api/v1/startBSync",
+                mensagem,HttpExtension.POST);
+        } catch (Exception e) {
+        }
+
+        return ResponseEntity.ok().build();
+    }
+}
